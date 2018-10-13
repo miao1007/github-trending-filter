dd6c30b9fc279d12d7664e397edda29f7e8127a2
miao1007 <miao1007@gmail.com>
2018-10-13 23:23:11 +0800

    support hide and show

---
+++ README.md
@@ -0,0 +1,8 @@
+# Features 
+Block any keys you may not like.
+
+# Install 
+
+1. Install from https://tampermonkey.net
+2. [Install](https://github.com/miao1007/github-trending-filter/raw/master/trendingfilter.user.js) the script 
+3. Edit the script and modify the keywords you may concern in `hideByFilter`.
--- trendingfilter.user.js
+++ trendingfilter.user.js
@@ -12,24 +12,46 @@
     'use strict';
     // for https://github.com/vitalets/github-trending-repos/issues/120
     const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
-    
+
+    const NON_TECH_WORDS = /interview|blog/
+
     /**
      * remove element by keywords.
      * @param {Array} keywords regexp list
      */
     function hideByFilter(keywords){
-        var repos = window.document.querySelector('.repo-list').querySelectorAll('li');
+        var repoList = window.document.querySelector('.repo-list');
+        var repos = repoList.querySelectorAll('li');
+        var hiddenRepos = new Set();
+        var show = function(e){
+            repoList.style.display=""
+            hiddenRepos.forEach(hiden=>{
+                hiden.setAttribute("style", "background-color:#FFD2D2");
+            })
+            // hide self
+            e.currentTarget.parentElement.setAttribute("style","display: none !important")
+        }
         repos.forEach(repo=>{
             var text = repo.textContent.replace(/[\s\n\r]+/gm,' ');
             keywords.forEach(regex => {
                 if(regex.test(text)){
-                    repo.remove();
-                    debugger;
-                    console.log(repo.querySelector('a').textContent + " was filtered")
+                    hiddenRepos.add(repo);
+                    repo.style.display = "none !important"
                 }
             })
         })
+        if(hiddenRepos.size){
+            var div = document.createElement("div");
+            div.className += "col-12 d-block width-full py-4 border-bottom";
+            repoList.insertBefore(div, repoList.children[0]);
+            div.appendChild(document.createTextNode("Some results were removed by the Github Trending Filter extension ("))
+            var a = document.createElement("a");
+            a.textContent = 'show';
+            a.onclick=show;
+            div.appendChild(a)
+            div.appendChild(document.createTextNode(').'))
+        }
     }
 
-    hideByFilter([REGEX_CHINESE])
+    hideByFilter([REGEX_CHINESE, NON_TECH_WORDS])
 })();
